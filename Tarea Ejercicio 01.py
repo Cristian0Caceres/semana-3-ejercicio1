d813e1739f2113c9ce676ffbddcb81b844d5e68c
Cristian0Caceres <134634536+Cristian0Caceres@users.noreply.github.com>
2023-09-04 11:26:30 -0300

    tarea parte 1 ejercicio 1

---
+++ Tarea Ejercicio 01.py
@@ -0,0 +1,21 @@
+import math
+class circulo:
+    def __init__(self,radio):
+        self.radio=radio
+    def calcula_area(self):
+        area=math.pi * self.radio**2
+        print(f"el areea es {area}")
+    def calcular_perimetro(self):
+        perimetro=(2*math.pi)*self.radio
+        print(f"el perimetro es {perimetro}")
+    def modifica_radio(self):
+        nuevo_radio=int(input("inserte un nuevo radio: "))
+        self.radio = nuevo_radio
+circulotest=circulo(6)
+ciclo=True
+Ciclos=0
+circulotest=circulo(6)
+while ciclo==True:
+    circulotest.calcula_area()
+    circulotest.calcular_perimetro()
+    circulotest.modifica_radio()
